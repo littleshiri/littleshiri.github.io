9035e5384db0aab4781fcc5c8a09d8911765786c
littleshiri <wnsdh10@naver.com>
2016-01-15 20:12:27 +0900

    asdfasdf

---
+++ data/gameData.js
@@ -0,0 +1,9 @@
+var data = 
+[
+{
+	"year":2015,
+	"title":"실험용 예제"
+}
+];
+
+articulate(data);
--- js/gameitem.js
+++ js/gameitem.js
@@ -1,26 +1,19 @@
 var container = document.getElementById("container");
-var data={
-	"title":123
-};
-var request = new XMLHttpRequest();
 
 function game(d){
 	var html = '<div class="gameitem">' + d.title + '</div>' ;
-	container.innerHTML+=html;
 	return html;
 }
 
-var xmlhttp = new XMLHttpRequest();
-var url = "data/gameData.json";
-
-xmlhttp.onreadystatechange = function() {
-if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    var myArr = JSON.parse(xmlhttp.responseText);
-    game(myArr);
-    }
-};
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-game(data)
+function articulate(data){
+	var nowyear = 0
+	var html = ""
+	for (var d in data){
+		if(nowyear!=d.year){
+			nowyear=d.year;
+			html += '<h1>' + nowyear + '</h1>'
+		}
+		html += game(d);
+	}
+	container.innerHTML += html;
+}
