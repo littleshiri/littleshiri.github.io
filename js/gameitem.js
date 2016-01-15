d5d4d7481c8bc71ad91bb9068cbf6c79d06089f0
littleshiri <wnsdh10@naver.com>
2016-01-15 17:03:02 +0900

    asdf

--- js/gameitem.js
+++ js/gameitem.js
@@ -6,12 +6,12 @@ function game(title){
 	var html = '<div class="gameitem">' + title + '</div>' ;
 	return html;
 }
-function parser(){
+function parser(e){
+	alert("loaded");
 	data = JSON.parse(this.requestText);
-	alert(data);
 	container.innerHTML += data;
 }
 
-request.open('GET', 'gameDATA.json', true);
+request.open('GET', './gameDATA.json', true);
 request.send();
 request.addEventListener('load',parser);
