33fb7d72bb7bbeb3c8e2b601f901ff1c090d7f84
littleshiri <wnsdh10@naver.com>
2016-01-15 16:56:58 +0900

    json

---
+++ js/gameitem.js
@@ -0,0 +1,17 @@
+var container = document.getElementById("container");
+var data;
+var request = new XMLHttpRequest();
+
+function game(title){
+	var html = '<div class="gameitem">' + title + '</div>' ;
+	return html;
+}
+function parser(e){
+	data = JSON.parse(this.requestText);
+	alert(data);
+	container.innerHTML += data;
+}
+
+req.open('GET', 'gameDATA.json', true);
+req.send();
+req.addEventListener('load',parser);
