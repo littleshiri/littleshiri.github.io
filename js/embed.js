26090e3e6ade55b327ef4a63f9b8a43a7426a274
littleshiri <wnsdh10@naver.com>
2016-01-16 14:53:55 +0900

    js tail

--- js/embed.js
+++ js/embed.js
@@ -1,6 +1,10 @@
 function draw(file,w,h){
+	var tailscript = '<div id="tail"><a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="크리에이티브 커먼즈 라이선스" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png" /></a><br/>이 저작물은 <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">크리에이티브 커먼즈 저작자표시-변경금지 4.0 국제 라이선스</a>에 따라 이용할 수 있습니다.</div>';
 	var html='<table style="width:100%;height:100%;position:fixed;"><tr><td><div align="center">';
 	html+='<embed src="' + file + '" width="' + w + '" height="' + h + '">';
-	html+='</div></td></tr></table>';
+	html+='</div>';
+	html+=tailscript;
+	html+='</td></tr></table>';
 	document.body.innerHTML += html;
+	document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="../normal.css"/>';
 }
